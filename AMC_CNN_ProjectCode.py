1388f3f6452e6d89a5e799673afb9118061d9e8d
Amith R <amith4504@gmail.com>
2019-04-25 08:52:28 +0530

    Add files via upload

--- AMC_CNN_ProjectCode.py
+++ AMC_CNN_ProjectCode.py
@@ -19,7 +19,7 @@ from keras.regularizers import *
 from keras.optimizers import adam
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pickle,random,sys,keras # for unpickling
+import pickle,sys,keras # for unpickling
 
 
 
@@ -72,26 +72,29 @@ Y_test = to_onehot(one_hot2int)
 # training is done by random samples from the dataset with a total of 110000 vectors 
 # and is tested on all test cases in dataset
 
-in_shp = list((X_train.shape[1:]))
-print (X_train.shape,in_shp)
+in_shp = list((X_train.shape[1:])) #[2,128]
+print (X_train.shape,in_shp) 
 classes = mods
 
 # BUILDING THE NEURAL NETWORK ARCHITECTURE
 drop_out = 0.5
 model = models.Sequential() # from keras library
 # INPUT LAYER 2 x 128
-model.add(Reshape([1]+in_shp,input_shape=in_shp))
-model.add(ZeroPadding2D((0,2)))
+# Reshape(self,target_shape,**kwargs)
+model.add(Reshape([1]+in_shp,input_shape=in_shp)) # no change in shape in this command
+model.add(ZeroPadding2D((0,1)))
 # ZERO PADDING required to make output size equal to input size
-model.add(Convolution2D(256,1,3,border_mode='valid',activation="relu",name="conv1",init='glorot_uniform'))
+
+model.add(Convolution2D(256,kernel_size=(3,1),strides=(1,1),border_mode='valid',activation="relu",name="conv1",init='glorot_uniform',data_format='channels_first'))
 model.add(Dropout(drop_out))
 # Dropout is required to prevent overfitting 
 
 #LAYER 2
 model.add(ZeroPadding2D((0,2)))
-model.add(Convolution2D(80,2,3,border_mode="valid",activation="relu",name="conv2",init = 'glorot_uniform'))
-model.add(Dropout(drop_out))
 
+model.add(Convolution2D(80,kernel_size=(3,2),strides=(1,1),border_mode="valid",activation="relu",name="conv2",init = 'glorot_uniform',data_format='channels_first'))
+model.add(Dropout(drop_out))
+#
 model.add(Flatten())
 #DENSE LAYER OR FULLY CONNECTED LAYER
 model.add(Dense(256,activation='relu',init='he_normal',name="dense1"))
@@ -99,30 +102,97 @@ model.add(Dropout(drop_out))
 # 2nd dense layer
 model.add(Dense(len(classes),init='he_normal',name="dense2"))
 model.add(Activation('softmax'))
+
 model.add(Reshape([len(classes)]))
 
 model.compile(loss='categorical_crossentropy',optimizer='adam')
+# ADAM optimizer provides gradient normalization and momentum which reduces the importance of
+#learning rate.
 model.summary()
 
 nb_epoch = 100
-batch_size = 1024
+batch_size = 512 #1024
 
 filepath='CNN_AMC.wts.h5'
 
+
+""" callbacks is a set of functions to be applied at given stages during training
+   callbacks.Modelcheckpoint is used to save the model after every epoch . The model checkpoints will be saved with epoch
+   number and the validation loss in the filename """ 
 history =model.fit(X_train,
                    Y_train,
                    batch_size=batch_size,
                    nb_epoch=nb_epoch,
-                   show_accuracy=False,
-                   verbose=2,
+                   verbose=1,
                    validation_data=(X_test,Y_test),
                    callbacks=[keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True, mode='auto'),
         keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='auto')])
 
-model.load_weights(filepath)
+# reloading the best weights after the training is finished.
+model.load_weights(filepath) # returns the trained tensors as list of numpy arrays
+
+# model is trained, accuracy is shown ,loss curves are plotted, confusion matrix are to be created.
+plt.figure()
+plt.title('Training performance')
+plt.plot(history.epoch, history.history['loss'], label='train loss+error')
+plt.plot(history.epoch, history.history['val_loss'], label='val_error')
+plt.legend()
 
 
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues, labels=[]):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(labels))
+    plt.xticks(tick_marks, labels, rotation=45)
+    plt.yticks(tick_marks, labels)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    
+test_Y_hat = model.predict(X_test, batch_size=batch_size)
+conf = np.zeros([len(classes),len(classes)])
+confnorm = np.zeros([len(classes),len(classes)])
+for i in range(0,X_test.shape[0]):
+    j = list(Y_test[i,:]).index(1)
+    k = int(np.argmax(test_Y_hat[i,:]))
+    conf[j,k] = conf[j,k] + 1
+for i in range(0,len(classes)):
+    confnorm[i,:] = conf[i,:] / np.sum(conf[i,:])
+plot_confusion_matrix(confnorm, labels=classes) 
+ 
 
+acc = {}
+for snr in snrs:
 
+    # extract classes @ SNR
+    test_SNRs = map(lambda x: lbl[x][1], test_idx)
+    test_X_i = X_test[np.where(np.array(test_SNRs)==snr)]
+    test_Y_i = Y_test[np.where(np.array(test_SNRs)==snr)]    
 
+    # estimate classes
+    test_Y_i_hat = model.predict(test_X_i)
+    conf = np.zeros([len(classes),len(classes)])
+    confnorm = np.zeros([len(classes),len(classes)])
+    for i in range(0,test_X_i.shape[0]):
+        j = list(test_Y_i[i,:]).index(1)
+        k = int(np.argmax(test_Y_i_hat[i,:]))
+        conf[j,k] = conf[j,k] + 1
+    for i in range(0,len(classes)):
+        confnorm[i,:] = conf[i,:] / np.sum(conf[i,:])
+    plt.figure()
+    plot_confusion_matrix(confnorm, labels=classes, title="ConvNet Confusion Matrix (SNR=%d)"%(snr))
+    plt.savefig(snr,dpi=400)
+    cor = np.sum(np.diag(conf))
+    ncor = np.sum(conf) - cor
+    #print "Overall Accuracy: ", cor / (cor+ncor)
+    acc[snr] = 1.0*cor/(cor+ncor)   
 
+print acc
+fd = open('results_cnn2_d0.5.dat','wb')
+cPickle.dump( ("CNN2", 0.5, acc) , fd )
+
+plt.plot(snrs, map(lambda x: acc[x], snrs))
+plt.xlabel("Signal to Noise Ratio")
+plt.ylabel("Classification Accuracy")
+plt.title("CNN2 Classification Accuracy on RadioML 2016.10 Alpha")
